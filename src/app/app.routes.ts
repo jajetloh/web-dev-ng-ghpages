48add3f11d347aca317b6edb0cbc51159e7daab5
Ja-Jet Loh <jajet.codes@gmail.com>
2024-04-09 04:32:09 +0800

    add initial content

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
+import { HomeComponent } from "./home/home.component"
+import { TutorialComponent } from "./tutorial/tutorial.component"
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'tutorial',
+    component: TutorialComponent,
+  }
+]
